d368243479c9cc69aad63aadc7afc9f11af110ca
GunGithub2019 <57030862+GunGithub2019@users.noreply.github.com>
2019-10-26 15:50:19 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'iIwb7oxLtURRC2WD+92cw0mc89H6rQM3nDqYDpPMAatDRxLN2SI2zRnTX6M2wVtmWbr0PIUrkUA9FIpHivxmBOB6iFcCmgFTJwyqASu+sicXXG9fZ9kty8HdEpHNlYZXhImeOakMPEpraHEaHUPoCwdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
